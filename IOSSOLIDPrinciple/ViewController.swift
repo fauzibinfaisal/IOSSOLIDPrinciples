f68349582e1d45b258c6d1140c51bc0081508368
Fauzi <fauzibinfaisal@gmail.com>
2021-01-15 13:41:06 +0700

    Initial Commit

---
+++ IOSSOLIDPrinciple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSSOLIDPrinciple
+//
+//  Created by Fauzi Fauzi on 15/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
